c1d5801c877606678db43f36d4844a34ee244c42
Kaung Khant Win <kkw.kaungkhant@gmail.com>
2022-01-19 15:44:35 +0630

    Chapter-3

---
+++ src/Chapter_3/ProgrammingExercises/MetricConversion.java
@@ -0,0 +1,26 @@
+package Chapter_3.ProgrammingExercises;// MetricConversion.java
+// Chapter 3,Exercise 7
+import java.util.Scanner;
+
+import static Chapter_3.ProgrammingExercises.BookstoreCredit.convertToLiters;
+
+public class MetricConversion
+{
+  public static void main (String args[])
+  {
+     int aValue;
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter an integer >> ");
+     aValue = input.nextInt();
+     convertToCentimeters(aValue);
+     convertToLiters(aValue);
+  }
+
+  public static void convertToCentimeters(int inches)
+  {
+     double centimeters;
+     final double CENT_FACTOR = 2.54;
+     centimeters = inches * CENT_FACTOR;
+     System.out.println(inches + " inches is " + centimeters + " centimeters");
+  }
+}
